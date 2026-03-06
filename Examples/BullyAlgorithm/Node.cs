8ccf6ed33dee500966011e622f2138dab2d2eedd
Gabriel Castellani de Oliveira <gabriel.castellani@remotatec.com.br>
2026-03-06 20:56:51 -0300

    Inclusão de exemplo para algoritmo de bully

---
+++ Examples/BullyAlgorithm/Node.cs
@@ -0,0 +1,64 @@
+﻿namespace BullyAlgorithm
+{
+    public sealed class Node
+    {
+        public int Id { get; private set; }
+        public bool IsAlive { get; set; } = true;
+        public bool IsCoordinator { get; set; } = false;
+
+        private List<Node> _nodes;
+
+        public Node(int id)
+        {
+            Id = id;
+        }
+
+        public void SetCluster(List<Node> nodes)
+        {
+            _nodes = nodes;
+        }
+
+        public void StartElection()
+        {
+            Console.WriteLine($"Node ID: {Id} - Iniciando a eleição");
+
+            var nodes = _nodes.Where(node => node.Id > Id && node.IsAlive);
+
+            if (!nodes.Any())
+                BecomeCoordinator();
+
+            foreach (var node in nodes)
+            {
+                Console.WriteLine($"Node {Id} enviou ELECTION para {node.Id}");
+                node.ReceiveElection(Id);
+            }
+        }
+
+        public void ReceiveElection(int nodeId)
+        {
+            Console.WriteLine($"Node {Id} recebeu ELECTION de {nodeId}");
+
+            if (IsAlive)
+            {
+                Console.WriteLine($"Node {Id} respondeu OK para {nodeId}");
+                StartElection();
+            }
+        }
+
+        public void BecomeCoordinator()
+        {
+            IsCoordinator = true;
+            Console.WriteLine($"Node {Id} virou o NOVO COORDENADOR");
+
+            foreach (var node in _nodes.Where(node => node.Id != Id && node.IsAlive))
+                node.ReceiveCoordinator(Id);
+        }
+
+        public void ReceiveCoordinator(int newCoordinator)
+        {
+            IsCoordinator = false;
+
+            Console.WriteLine($"Node {Id} reconhece {newCoordinator} como coordenador");
+        }
+    }
+}
---
+++ Examples/BullyAlgorithm/Program.cs
@@ -0,0 +1,21 @@
+﻿using BullyAlgorithm;
+
+var nodes = new List<Node>
+{
+    new Node(1),
+    new Node(2),
+    new Node(3),
+    new Node(4),
+    new Node(5){ IsCoordinator = true },
+};
+
+foreach (var node in nodes)
+    node.SetCluster(nodes);
+
+Console.WriteLine("Líder inicial: Node 5");
+
+// Simula uma falha
+nodes.FirstOrDefault(item => item.Id == 5).IsAlive = false;
+
+Console.WriteLine("Node 2 detectou falha e iniciou eleição");
+nodes.First(n => n.Id == 2).StartElection();
